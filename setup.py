f6c7d09da23be7a1e45da31da053bd4372f71d60
DevelogBG <bani.ghosh10@gmail.com>
2026-03-22 17:34:15 +1100

    notebook experiment done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical-chatbot",
+    version = "0.1.0",
+    author= "Bani",
+    author_email = "banibrataghosh916@gmail.com",
+    packages= find_packages(),
+    install_requirements = []
+)
